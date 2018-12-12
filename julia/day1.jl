d7dd3c7e511d0ac6ddf3368176c13ed0b5305d2d
kanishkamisra <menogetusername@gmail.com>
2018-12-12 16:57:58 -0500

    day 1 done in julia

--- julia/day1.jl
+++ julia/day1.jl
@@ -1,11 +1,27 @@
 module day1
 	# part 1
-	input = split(read("data/day1.txt", String))
+	input = split(read("../data/day1.txt", String))
 
-	part1 = sum(map(x -> parse(Int64, x), input))
+  input_modified = map(x -> parse(Int64, x), input)
+
+	part1 = sum(input_modified)
 	println("Solution to Part 1: $part1")
 
 	# part2
-	current = 0
-	repeating_freqs = Set([current])
+	function part2(input = input_modified)
+    current = 0
+    repeating_freqs = Set{Int64}([0])
+
+    while true
+      for x in input_modified
+        current += x
+        if current in repeating_freqs
+          return(current)
+        end
+        push!(repeating_freqs, current)
+      end
+    end
+  end
+  part2_solution = part2()
+  println("Solution to part2: $part2_solution")
 end
